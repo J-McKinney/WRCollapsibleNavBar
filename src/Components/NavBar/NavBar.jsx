5da524409259b35bc551d42987d083e7c585c0bf
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-11 13:20:20 -0400

    yay

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,88 +2,45 @@ import React, { Component } from "react";
 import Style from "./NavBar.module.css";
 
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      show: true,
-      scrollPos: 0,
-    };
-    this.handleScroll = this.handleScroll.bind(this);
-  }
+  state = {
+    show: true,
+    scrollPos: 0,
+  };
 
   componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
+    // console.log("Did Mount: ", this.state.scrollPos);
+    window.addEventListener("scroll", this.onscroll);
   }
-
+  componentDidUpdate() {
+    // console.log("Did Update: ", this.state.scrollPos);
+  }
   componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
+    // console.log("Will Unmount: ", this.state.scrollPos);
+    window.removeEventListener("scroll", this.onscroll);
   }
 
-  handleScroll() {
+  onscroll = () => {
+    console.log("Scrolling: ", this.state.scrollPos);
     const { scrollPos } = this.state;
     this.setState({
       scrollPos: document.body.getBoundingClientRect().top,
       show: document.body.getBoundingClientRect().top > scrollPos,
     });
-  }
+  };
 
   render() {
     return (
       <>
-        <Transition>
-          <StyledNavbar className={this.state.show ? "active" : "hidden"}>
-            <a className="brand" href={this.props.brand.to}>
-              {this.props.brand.name}
-            </a>
-            <nav>
-              <div />
-            </nav>
-          </StyledNavbar>
-        </Transition>
+        <div className="navbarWrapper">
+          <div className={Style.navbar}>
+            <a href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </div>
       </>
     );
   }
 }
-
-const Transition = Style.div`
-  .active {
-    visibility: visible;
-    transition: all 200ms ease-in;
-  }
-  .hidden {
-    visibility: hidden;
-    transition: all 200ms ease-out;
-    transform: translate(0, -100%);
-  }
-`;
-const StyledNavbar = Style.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  margin: 0 auto;
-  height: 3rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  font-weight: bolder;
-  background: cornflowerblue;
-  z-index: 1000;
-  a {
-    margin-right: 1rem;
-    font-weight: normal;
-    color: black;
-    text-decoration: none;
-  }
-  .brand {
-    font-style: italic;
-    margin-left: 1rem;
-    font-weight: bold;
-    color: white;
-    font-size: 1.25rem;
-  }
-`;
 
 export default NavBar;
